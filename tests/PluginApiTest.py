2741cfcd7ddbd0e3b066e59ebcbd2dc1a62b6cbf
Andrew Liddle <aliddle98@gmail.com>
2023-03-21 19:25:32 -0700

    -added analyze_column(), which runs all plugins on a column of data and returns a dict of plugin names by confidence score keys - added additional documentation to PluginApi - added tests to PluginApiTest

--- tests/PluginApiTest.py
+++ tests/PluginApiTest.py
@@ -1,3 +1,4 @@
+from ast import Dict
 import unittest
 import sys
 sys.path.append("..")
@@ -17,9 +18,9 @@ class TestPluginApi(unittest.TestCase):
         """
         test = PluginApi()
         actual = test.get_plugin_list()
-        print("API TEST discovered plugins:") # added for ease of test debugging
-        print(test.get_plugin_list()) # added for ease of test debugging
-        expected = ['GenericNumberPlugin', 'GenericTextPlugin', 'PhoneNumberPlugin'] # placeholder at the moment
+        # print("API TEST discovered plugins:") # added for ease of test debugging
+        # print(test.get_plugin_list()) # added for ease of test debugging
+        expected = ['GenericNumberPlugin', 'GenericTextPlugin', 'PhoneNumberPlugin', 'TestPluginOne', 'TestPluginTwo'] # placeholder at the moment
         self.assertListEqual(actual, expected)
 
     def test_plugin_function_exec(self):
@@ -32,6 +33,16 @@ class TestPluginApi(unittest.TestCase):
         errorHundred = 100.00
         self.assertNotEqual(actual, errorZero)
         self.assertNotEqual(actual, errorHundred)
+
+    def test_many_plugin_function_exec(self):
+        """ 
+        Tests whether the api can dynamically execute plugin functions on a column using multiple plugins, 
+        and if the undesired_plugs works as intended
+        """
+        test = PluginApi()
+        column = ['(555)555-555', '(555) 555-555', '+1555555555', '555.555.5555', '5555555555']
+        actual = test.analyze_column(column)
+        self.assertIs(type(actual), dict)
 
   
 

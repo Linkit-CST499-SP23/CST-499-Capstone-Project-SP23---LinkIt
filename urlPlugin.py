6be9ac8d756c22ab4b5f34921501713d76690728
Ethan Garnica <92120279+ethan-garnica@users.noreply.github.com>
2023-04-12 14:35:08 -0700

    Add files via upload

---
+++ urlPlugin.py
@@ -0,0 +1,60 @@
+import re
+import math
+
+def get_confidence_score(col_list):      
+    scores = []
+    col = remove_null(col_list)
+    
+    
+    for c in col:
+        scores.append(get_elam_score(c))
+
+    scores = remove_outliers(scores)
+    confidence_score = math.floor(sum(scores) / len(scores))
+    return confidence_score     
+
+def get_elam_score(string):
+    
+    # matches URLs that begin "http", "https", "ftp"
+    if re.match(r'^(https?|ftp|sftp|file|mailto|tel|sms|data):\/\/', string):
+        score = 100.0
+    # Second, check if the string has a top-level domain (e.g. .com, .org, .net)
+    elif re.search(r'(^|\s)([a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*\.(?:com|net|org|edu|gov|io|co|biz|info|me|tv|us|ca|uk|au|de|jp))(?:\/[^\s?#]*)?(?:\?[^#\s]*)?(?:#[^\s]*)?(\s|$)', string):
+        score = 90.0
+    # Third, check if the string has a subdomain (e.g. www) also needs a domain and tld
+    elif re.search(r'^(www|blog|mail|smtp|pop|imap|ftp)\.[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}(\/[^\s?#]*)?(\?[^#\s]*)?(#[^\s]*)?$', string):
+        score = 80.0
+    # Fourth, check if the string is an IP address
+    elif re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', string):
+        score = 40.0
+    # Fifth, checks if string is formatted like a url but doesnt include scheme, common tlds, or subdomains 
+    elif re.search(r'^\S{2,}\.\S{2,}(\.\S{2,})?(?:\/\S{2,})?$', string):
+        score = 20.0
+    # If none of the above conditions are met, assign a score of 0
+    else:
+        score = 0.0
+    
+    return score
+
+def remove_null(col):
+    null_strings = ['NA', 'N/A', 'na', 'n/a', 'Na', 'N/a']
+    col = [elem for elem in col if elem is not None] # remove None values
+    col = [elem for elem in col if elem not in null_strings] # remove any strings denoting null values
+    return col
+
+def remove_outliers(scores):
+    outliers = set()
+    avg = sum(scores) / len(scores)
+    standard_deviation = (sum([(s - avg)**2 for s in scores]) / len(scores))**(1/2)
+
+    for s in scores: 
+        if (s < (avg - (standard_deviation*2)) or s > ((standard_deviation*2) + avg)):
+            outliers.add(s)
+
+    scores = [s for s in scores if s not in outliers]
+    return scores
+
+validPhoneNumList = ['https://www.test.com', 'http://www.example.com', 'example.com', 'sub.domain.example.ca/path/to/page?param=value#section',
+                                'www.example.org', 'blog.example.xyz/path/to/post', 'pop.example.ac', 'www.example.ai#section', 
+                                '192.168.0.100', 'mydomain.museum', '.example.com']
+print(get_confidence_score(validPhoneNumList))
---
+++ urlPluginTest.py
@@ -0,0 +1,58 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Linkit', 'plugins')))
+from urlPlugin import *
+
+class TestGetConfidenceScore(unittest.TestCase):
+    
+    def test_get_confidence_score(self):
+        # Test input list with various types of strings
+        input_list = [
+            'https://www.test.com',
+            'http://www.example.com',
+            'example.com',
+            'sub.domain.example.ca/path/to/page?param=value#section',
+            'www.example.org',
+            'blog.example.xyz/path/to/post',
+            'pop.example.ac',
+            'www.example.ai#section', 
+            '192.168.0.100',
+            'mydomain.museum',
+            '.example.com'
+        ]
+        
+        # Expected confidence score for the above input list
+        expected_score = 71.0
+        
+        # Call the function and check the result
+        self.assertEqual(get_confidence_score(input_list), expected_score)
+    
+    
+    
+    def test_get_elam_score(self):
+        # Test various types of input strings
+        input_string1 = 'https://www.test.com'
+        expected_score1 = 100.0
+        input_string2 = 'sub.domain.example.ca/path/to/page?param=value#section'
+        expected_score2 = 90.0
+        input_string3 = 'blog.example.xyz/path/to/post'
+        expected_score3 = 80.0
+        input_string4 = '192.168.0.100'
+        expected_score4 = 40.0
+        input_string5 = '.example.com'
+        expected_score5 = 20.0
+        input_string6 = 'not_a_url'
+        expected_score6 = 0.0
+        
+        # Call the function for each input string and check the result
+        self.assertEqual(get_elam_score(input_string1), expected_score1)
+        self.assertEqual(get_elam_score(input_string2), expected_score2)
+        self.assertEqual(get_elam_score(input_string3), expected_score3)
+        self.assertEqual(get_elam_score(input_string4), expected_score4)
+        self.assertEqual(get_elam_score(input_string5), expected_score5)
+        self.assertEqual(get_elam_score(input_string6), expected_score6)
+
+if __name__ == '__main__':
+    unittest.main()
+        

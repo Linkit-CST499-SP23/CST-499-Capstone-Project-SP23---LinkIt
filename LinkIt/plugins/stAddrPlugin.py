5f041f5480ae906182911a6ffbba7edbc0e3553e
Ethan Garnica <92120279+ethan-garnica@users.noreply.github.com>
2023-04-27 17:24:25 -0700

    Added comments and removed code that does nothing.

--- LinkIt/plugins/stAddrPlugin.py
+++ LinkIt/plugins/stAddrPlugin.py
@@ -4,8 +4,15 @@ import math
 
 def get_confidence_score(column_name, address_list):
     scores = []
+    
+    # if address_list is empty return 0
+    if len(address_list) == 0:
+        return 0
+
+    # matches possible column name case insensitive
     regex = re.compile("(street|address|destination|origin)", re.IGNORECASE)
     address_list = remove_null(address_list)
+
     for address in address_list:
         scores.append(get_elem_score(address))
 
@@ -13,15 +20,19 @@ def get_confidence_score(column_name, address_list):
         scores = remove_outliers(scores)
     
     avg_score = sum(scores) / len(scores) if len(scores) > 0 else 0
+    # if column name matches likely names multiply score by 1.1
     if regex.match(column_name):
         avg_score *= 1.1
+    # if column name does not match likely names multiply by 0.9
     else:
         avg_score *= .9
+    # if the average score is about 100 round it down to 100
     if avg_score > 100:
         avg_score = max(100.0, math.floor(avg_score / 100.0) * 100.0)
     return avg_score
+    
 def get_elem_score(string):
-    regex = re.compile("(street|address|destination|origin)", re.IGNORECASE)
+    # Main componentes of a street address
     addrComps = ['AddressNumber', 'StreetName', 'StreetNamePostType']
     currentAddr = []
     score = 0
@@ -29,19 +40,23 @@ def get_elem_score(string):
         parsed_address = usaddress.parse(string)
         for (value,component) in parsed_address:
             currentAddr.append(component)
-        
+        # First check, if the 3 of 3 main components of a street address match add 100 to list
         if len(set(currentAddr) & set(addrComps)) == 3:
             score = 100.0/1
+        # Second check, if the 2 of 3 main components of a street address match add 50 to list
         elif len(set(currentAddr) & set(addrComps)) == 2:
             score = 100.0/2
+        # Third check, if the 1 of 3 main components of a street address match add 25 to list
         elif len(set(currentAddr) & set(addrComps)) == 1:
             score = 100.0/4
+        # Fourth check, if the 0 of 3 main components of a street address match add 0 to list
         elif len(set(currentAddr) & set(addrComps)) == 0:
             score = 0.0
-
+    # error raised when a string is likely not a valid address that could be tested or tokens were labeled incorrectly
     except usaddress.RepeatedLabelError:
         pass
     return score
+
 def remove_null(col):
     null_strings = ['NA', 'N/A', 'na', 'n/a', 'Na', 'N/a']
     col = [elem for elem in col if elem is not None] # remove None values
@@ -58,6 +73,3 @@ def remove_outliers(scores):
 
     scores = [s for s in scores if s not in outliers]
     return scores
-
-
-

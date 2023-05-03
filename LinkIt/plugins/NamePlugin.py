fac71e3ed0126e91dd4e3b8bbbd7e58bf37c0ba5
RISHABH RAJASHEKAR SHETTY <92135071+RishabhRShetty@users.noreply.github.com>
2023-05-03 07:36:17 -0700

    Add files via upload

--- LinkIt/plugins/NamePlugin.py
+++ LinkIt/plugins/NamePlugin.py
@@ -2,78 +2,69 @@ import re
 
 """
 
-The get_confidence_score() function takes in a csv column name and column values 
-passed in as a list and returns a confidence score by taking an average
-on how likely each item in the column is a name.
+The get_confidence_score() function takes in a list of names and returns a 
+confidence score by taking an average on how likely each name is a valid name.
 
-input: string, string list
+input: string list
 output: double
 
 """
-def get_confidence_score(col_name, col_vals):
-    col_name_check = ("name" in col_name.lower())
-
+def get_confidence_score(names):
     scores = []
-    col_vals = remove_lead_trail_space(col_vals)
-    col_vals = remove_null(col_vals)
+    names = remove_lead_trail_space(names)
+    names = remove_null(names) 
 
-    for c in col_vals:
-        scores.append(get_elem_score(col_name_check, c))
-
+    for name in names:
+        scores.append(get_name_score(name))
     scores = remove_outliers(scores)
-    return sum(scores) / len(scores)
+
+    if (len(scores) == 0):
+        return 0.0
+    else:
+        return sum(scores) / len(scores)
 
 
 """
-The get_elem_score() function takes in a boolean denoting
-whether 'name' was found in the column name and 
-one element from the column list and returns a confidence score on
-how the element is a name.
+The get_name_score() function takes one name and returns a confidence score on
+how well the name is a valid name.
 
-input: bool, string
+input: string
 output: double
 
 """
-def get_elem_score(col_name_check, elem):
-    name_formats = ["first last", "last, first", "first middle last", "last, first middle", "first m. last"]
-
-    if col_name_check:
-        score_multiplier = 1.0
+def get_name_score(name):
+    if (re.fullmatch("[A-Z][a-z]* [A-Z][a-z]*", name)):
+        return 100.0 
+    elif (re.fullmatch("[A-Z][a-z]* [A-Z][a-z]* [A-Z][a-z]*", name)):
+        return 80.0
     else:
-        score_multiplier = 0.5
-
-    name = elem.strip().lower()
-    for format in name_formats:
-        if re.match("^" + format.replace(".", "\.") + "$", name):
-            return 100.0 * score_multiplier
-
-    return 0.0
-
+        return 0.0
+    
 
 """
-The remove_null() function removes any null elements that may be in the column list.
+The remove_null() function removes any null elements that may be in the list of names.
 
 input: string list
 output: string list
 
 """
-def remove_null(col_vals):
+def remove_null(names):
     null_strings = ['NA', 'N/A', 'na', 'n/a', 'Na', 'N/a']
-    col_vals = [elem for elem in col_vals if elem is not None] # remove None values
-    col_vals = [elem for elem in col_vals if elem not in null_strings] # remove any strings denoting null values
-    return col_vals
+    names = [name for name in names if name is not None] # remove None values
+    names = [name for name in names if name not in null_strings] # remove any strings denoting null values
+    return names
 
 
 """
-The remove_lead_trail_space() function removes any leading and trailing spaces that may be in the strings of the column list.
+The remove_lead_trail_space() function removes any leading and trailing spaces that may be in the strings of the list of names.
 
 input: string list
 output: string list
 
 """
-def remove_lead_trail_space(col_vals):
-    col_vals = [elem.strip() for elem in col_vals] # remove leading and trailing spaces
-    return col_vals
+def remove_lead_trail_space(names):
+    names = [name.strip() for name in names] # remove leading and trailing spaces
+    return names
 
 
 """
@@ -85,11 +76,14 @@ output: double list
 
 """
 def remove_outliers(scores):
+    if (len(scores) == 0):
+        return scores
+    
     outliers = set()
     avg = sum(scores) / len(scores)
     standard_deviation = (sum([(s - avg)**2 for s in scores]) / len(scores))**(1/2)
 
-    for s in scores:
+    for s in scores: 
         if (s < (avg - (standard_deviation*2)) or s > ((standard_deviation*2) + avg)):
             outliers.add(s)
 

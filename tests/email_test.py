1bd3968572a75a5541296d02dcf4eb82bb228c74
Nyra Usi <nyrausi@gmail.com>
2023-05-04 12:04:25 -0700

    Changed function name.

--- tests/email_test.py
+++ tests/email_test.py
@@ -11,7 +11,7 @@ class TestEmailConfidence(unittest.TestCase):
         column = 'email'
         values = ['user1@example.com', 'user2@example.com', 'user3@example.com']
         expected_score = 100.0
-        actual_score = calculate_email_confidence(column, values)
+        actual_score = get_confidence_score(column, values)
         self.assertEqual(actual_score, expected_score)
 
     def test_mixed_emails(self):
@@ -20,21 +20,21 @@ class TestEmailConfidence(unittest.TestCase):
         expected_score = 100.0
         cleaned_column = remove_null(column)
         print(cleaned_column)  # add this line
-        actual_score = calculate_email_confidence(column, cleaned_column)
+        actual_score = get_confidence_score(column, cleaned_column)
         self.assertAlmostEqual(actual_score, expected_score)
 
     def test_no_emails(self):
         column = 'email'
         values = ['not_an_email1', 'not_an_email2', 'not_an_email3']
         expected_score = 0
-        actual_score = calculate_email_confidence(column, values)
+        actual_score = get_confidence_score(column, values)
         self.assertEqual(actual_score, expected_score)
 
     def test_empty_column(self):
         column = 'email'
         values = []
         expected_score = 0
-        actual_score = calculate_email_confidence(column, values)
+        actual_score = get_confidence_score(column, values)
         self.assertEqual(actual_score, expected_score)
 
 if __name__ == '__main__':
